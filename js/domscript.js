ab50db5e29640c449bae4a6c2ba7fec66e53fa5c
Daniel Büchele <daniel@buechele.cc>
2014-06-23 12:34:32 +0200

    init

---
+++ js/domscript.js
@@ -0,0 +1,91 @@
+var server;
+var reload_timer;
+var animation_timer;
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'kult2014',
+    clientId: 'f277c99d618f4baf863906716c305c83',
+    resolution: 'standard_resolution',
+    useHttp: true,
+    sortBy: 'most-recent',
+    mock: true,
+    success: function (data) {loadImages(data);}
+});
+
+$(function(){
+	
+	db.open( {
+	    server: 'instawall',
+	    version: 1,
+	    schema: {
+	        images: {
+	            key: { keyPath: 'id'},
+	            // Optionally add indexes
+	            indexes: {
+	                created_time: { }
+	            }
+	        }
+	    }
+	} ).done( function ( s ) {
+	    server = s;
+	} );
+	
+	triggerAnimation();
+	
+	animation_timer = setInterval(function () {
+    	console.log("trigger animation");
+    	triggerAnimation();
+    }, 30000);
+	
+	reload_timer = setInterval(function () {
+    	console.log("reload from instagram");
+    	feed.run();
+	}, 30000);
+
+});
+
+function triggerAnimation() {
+    
+    squares = $(".square").length;
+    doors = $(".door").length;
+    
+    
+    
+}
+
+function loadImages(data) {
+    for (var i = 0; i < data.data.length; i++) {
+        getBase64FromImage(data.data[i]);
+    }
+}
+
+function getBase64FromImage(obj) {
+    server.get('images',obj.id).done( function ( results ) {
+        if(typeof results != 'undefined') {
+            console.log("image from cache");
+        } else {
+            //load iamge
+            var img = new Image();
+            img.src = obj.images.standard_resolution.url;
+            
+            img.onload = function () {
+                var canvas = document.createElement("canvas");
+                canvas.width = this.width;
+                canvas.height = this.height;
+                
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(this, 0, 0);
+                
+                var dataURL = canvas.toDataURL("image/png").replace(/^data:image\/(png|jpg);base64,/, "");
+                obj.images.standard_resolution.data = dataURL;
+                
+                //$("div").append('<img src="data:image/png;base64,' + dataURL + '" />');
+                
+                server.images.add( obj ).done( function ( item ) {
+                    // item stored
+                    console.log("new image stored to cache");
+                }); 
+            }
+        }
+    });
+}
